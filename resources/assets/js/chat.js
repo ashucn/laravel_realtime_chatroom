6bef33b87593c1854a4451076bffe9ad26a559e0
MayLiu <ashucn@gmail.com>
2017-08-16 19:30:30 -0700

    update 手机iOS浏览器报错，不能使用es6

--- resources/assets/js/chat.js
+++ resources/assets/js/chat.js
@@ -5,3 +5,59 @@ Vue.component('chat-message', require('./components/ChatMessage.vue'));
 Vue.component('chat-composer', require('./components/ChatComposer.vue'));
 Vue.component('chat-log', require('./components/ChatLog.vue'));
 
+
+const app = new Vue({
+    el: '#app',
+    data: {
+        messages: []
+    },
+    methods: {
+        addMessage(message){
+            // post a message to db
+            axios.post('/messages', message).then(response => {
+                message.created_at = response.data.created_at;
+                this.messages.push(message);
+                $("html, body").animate({ scrollTop: $(document).height() }, "fast");
+            });
+        }
+    },
+    created: function(){
+        console.log("test echo");
+        // Echo.private(‘ashuchatroom’);
+        Echo.private('ashuchatroom')
+            .listen('MessagePosted', (event) => {
+                console.log(event);
+                // handle event
+                let msg = {
+                    message: event.message.message,
+                    user: {
+                        email: event.user.email
+                    }
+                }
+
+                console.log(userEmail, msg.user.email);
+                    this.messages.push(msg);
+
+                $("html, body").animate({ scrollTop: $(document).height() }, "fast");
+
+            });
+
+        // get all messages from db
+        axios.get('/messages').then(response => {
+            this.messages = response.data;
+        });
+        var userEmail = localStorage.getItem('useremail');
+        if(userEmail == null || userEmail != "{{Auth::user()->email}}"){
+            localStorage.setItem('useremail', "{{Auth::user()->email}}");
+        }
+
+    }
+});
+
+toastr.options = {
+  "positionClass": "toast-top-center"
+}
+
+$(document).ready(function(){
+    $("html, body").animate({ scrollTop: $(document).height() }, "fast");
+});
--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -26,67 +26,6 @@
 <script src="{{mix('/js/chat.js')}}"></script>
 <script>
 
-var userEmail = localStorage.getItem('useremail');
-const app = new Vue({
-    el: '#app',
-    data: {
-        messages: []
-    },
-    methods: {
-        addMessage(message){
-            message.user = { email: "{{Auth::user()->email}}" };
-            // post a message to db
-            axios.post('/messages', message).then(function(response){
-                message.created_at = response.data.created_at;
-                if(userEmail == message.user.email){
-                    this.messages.push(message);
-                }
-                $("html, body").animate({ scrollTop: $(document).height() }, "fast");
-            });
-        }
-    },
-    created: function(){
-        console.log("test echo");
-        // Echo.private(‘ashuchatroom’);
-        Echo.private('ashuchatroom')
-            .listen('MessagePosted', function (event){
-                console.log(event);
-                // handle event
-                var msg = {
-                    message: event.message.message,
-                    user: {
-                        email: event.user.email
-                    }
-                }
-
-                console.log(userEmail, msg.user.email);
-                if(userEmail != msg.user.email){
-                    this.messages.push(msg);
-                }
-
-                $("html, body").animate({ scrollTop: $(document).height() }, "fast");
-
-            });
-
-        // get all messages from db
-        axios.get('/messages').then(function(response) {
-            this.messages = response.data;
-        });
-        var userEmail = localStorage.getItem('useremail');
-        if(userEmail == null || userEmail != "{{Auth::user()->email}}"){
-            localStorage.setItem('useremail', "{{Auth::user()->email}}");
-        }
-
-    }
-});
-
-toastr.options = {
-  "positionClass": "toast-top-center"
-}
-
-$(document).ready(function(){
-    $("html, body").animate({ scrollTop: $(document).height() }, "fast");
-});
 
 </script>
 
